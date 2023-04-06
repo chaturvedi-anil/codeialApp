54a21068a2f443df2f73250faa8a03334aa97218
chaturvedi-anil <chaturvedia560@gmail.com>
2023-04-07 00:05:20 +0530

    layout added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const port = 8000;
 const app= express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 // this is for routes, it will automatically fetches the index.js in routes folder
 app.use('/', require('./routes'));
@@ -8,6 +11,7 @@ app.use('/', require('./routes'));
 // setup view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 
 app.listen(port, function(err)
 {
