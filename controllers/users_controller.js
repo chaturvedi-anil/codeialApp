252706e831b0b76f6265250e3eb001f37492dec7
chaturvedi-anil <chaturvedia560@gmail.com>
2023-04-16 20:34:17 +0530

    setup is user profile is present then delete first and update new one

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs= require(`fs`);
+const path = require('path');
 
 module.exports.profile = async function(req, res)
 {
@@ -36,8 +38,12 @@ module.exports.update= async function(req, res)
 
                 if(req.file)
                 {
+                    if(user.avatar)
+                    {
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
                     // this is the saving the path of uploaded file into the avatar field in the user
-                    user.avatar = User.avatarPath+ '/' + req.file.filename;
+                    user.avatar = User.avatarPath + req.file.filename;
                 }
 
                 //this will save the updated details in db 
