ea2f91b981d7df402c634e3cef30ab3f69b2d3ed
chaturvedi-anil <chaturvedia560@gmail.com>
2023-04-17 23:49:14 +0530

    setup posts api for sending data as json object

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,50 @@
-module.exports.index = function(req, res)
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res)
 {
+    // populate the user of each post 
+    let post= await Post.find({})
+    .sort('-createdAt')
+    .populate({
+        path: 'user',
+        // select all field except password
+        select: '-password'
+    })
+    .populate(
+    {
+        path: 'comments',
+        populate: {
+            path: 'user',
+            select: '-password'           
+        }
+    });
+
     return res.json({
         message: 'list of post',
-        posts: []
+        posts: post
     });
+}
+
+// delete
+module.exports.destroy= async function(req, res)
+{
+    try
+    {
+        let post = await Post.findById(req.params.id);
+        post.deleteOne(post._id, {projection: { password: 0 }});
+        // delete all the comments associated to that post
+        await Comment.deleteMany({post: req.params.id});
+        return res.json(200,
+            {
+            message:'post and associate comment delete'
+        });
+    }
+    catch(err)
+    {
+        console.log(err);
+        return res.json(500,{
+            message:'Internal server error'
+        });
+    };
 }
--- controllers/api/v2/posts_api.js
+++
@@ -1,7 +0,0 @@
-module.exports.index = function(req, res)
-{
-    return res.json({
-        message: 'v2 list of posts',
-        posts: []
-    });
-}
