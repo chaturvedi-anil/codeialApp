a870d2102caa86cb262c968ba886b3db693b066f
chaturvedi-anil <chaturvedia560@gmail.com>
2023-04-21 19:41:36 +0530

    setup schema for likes on posts and comments

--- models/post.js
+++ models/post.js
@@ -12,10 +12,18 @@ const postSchema = new mongoose.Schema({
         ref: 'User'
     },
     //include the array of ids of all comments in this post schema itself
-    comments:[
+    comments:
+    [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
+        }
+    ],
+    likes:
+    [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like'
         }
     ] 
 },{
